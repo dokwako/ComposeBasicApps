50ab3a5605850e00348b4945e2c72db100593ede
dokwako <denzilwawire418@gmail.com>
2024-10-31 18:58:32 +0300

    initial commit

--- app/src/main/java/com/example/basiccodelab/MainActivity.kt
+++ app/src/main/java/com/example/basiccodelab/MainActivity.kt
@@ -4,8 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -35,26 +39,39 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Surface(color = MaterialTheme.colorScheme.primary){
-        Text(
-            text = "Hello $name!",
-            modifier = modifier
-                .padding(24.dp)
-        )
+    Surface(
+        color = MaterialTheme.colorScheme.primary,
+        modifier = modifier
+            .padding(vertical = 4.dp, horizontal = 8.dp)
+    ){
+        Row(modifier= Modifier.padding(24.dp)){
+            Column(modifier = modifier.weight(1f)) {
+                Text(text = "Hello")
+                Text(text = name)
+            }
+            ElevatedButton(
+                onClick = {/* TODO*/}
+            ) {
+                Text("Show more")
+            }
+
+        }
     }
 }
 
 @Composable
-fun Myapp(modifier: Modifier = Modifier){
-    Surface(
-        modifier = modifier,
-        color =MaterialTheme.colorScheme.background
-    ){
-        Greeting("Android")
+fun Myapp(
+    modifier: Modifier = Modifier,
+    names: List<String> = listOf("World", "compose")
+){
+    Column(modifier = modifier.padding(vertical = 4.dp,)) {
+        for (name in names) {
+            Greeting("Android")
+        }
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true , widthDp = 320)
 @Composable
 fun GreetingPreview() {
     BasicCodelabTheme {
