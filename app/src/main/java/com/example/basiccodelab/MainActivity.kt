ebb35511dc546747ca1f832397ab278cd35c56e7
dokwako <denzilwawire418@gmail.com>
2024-11-01 15:12:31 +0300

    state compose

--- app/src/main/java/com/example/basiccodelab/MainActivity.kt
+++ app/src/main/java/com/example/basiccodelab/MainActivity.kt
@@ -10,6 +10,8 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
@@ -20,6 +22,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -47,8 +50,8 @@ class MainActivity : ComponentActivity() {
 
 
 @Composable
-fun Myapp(modifier: Modifier = Modifier) {
-    var shouldShowOnboarding by remember { mutableStateOf(true) }
+fun Myapp(modifier:Modifier = Modifier) {
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
 
     Surface {
         if (shouldShowOnboarding) {
@@ -86,10 +89,10 @@ fun OnboardingScreen(
 @Composable
 private fun Greetings(
     modifier: Modifier =Modifier,
-    names: List<String> = listOf("World", "compose")
+    names: List<String> = List(1000) { "$it"}
 ){
-    Column(modifier = modifier.padding(vertical = 4.dp,)) {
-        for (name in names) {
+    LazyColumn(modifier = modifier.padding(vertical = 4.dp,)) {
+        items (items = names) { name ->
             Greeting(name= name)
         }
     }
@@ -105,7 +108,7 @@ fun OnboardingPreview(){
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    val expanded = remember { mutableStateOf(false) }
+    val expanded = rememberSaveable  { mutableStateOf(false) }
     val extraPadding = if (expanded.value) 48.dp else 0.dp
     Surface(
         color = MaterialTheme.colorScheme.primary,
