4e77e2ae8c2a464c85755a98d69fceb91ff015a2
dokwako <denzilwawire418@gmail.com>
2024-11-01 23:39:58 +0300

    styling and theming app

--- app/src/main/java/com/example/basiccodelab/ui/theme/Theme.kt
+++ app/src/main/java/com/example/basiccodelab/ui/theme/Theme.kt
@@ -9,18 +9,24 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.ViewCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    surface = Blue,
+    onSurface = Navy,
+    primary = Navy,
+    onPrimary = Chartreuse
 )
-
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    surface = Blue,
+    onSurface = Blue,
+    primary = LightBlue,
+    onPrimary = Navy
+)
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -31,7 +37,7 @@ private val LightColorScheme = lightColorScheme(
     onBackground = Color(0xFF1C1B1F),
     onSurface = Color(0xFF1C1B1F),
     */
-)
+
 
 @Composable
 fun BasicCodelabTheme(
@@ -48,6 +54,13 @@ fun BasicCodelabTheme(
 
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
+    }
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            (view.context as Activity).window.statusBarColor = colorScheme.primary.toArgb()
+            ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars =darkTheme
+        }
     }
 
     MaterialTheme(
